b1cfead02811e7f4f25bd9bfcf5ad79ec16a3d98
jose-ant-1 <jgonsol1510@g.educaand.es>
2026-05-10 17:36:41 +0200

    cambio en la ruta y navbar

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://filter-suggestions-pmid-hash.trycloudflare.com/api',
+  apiUrl: 'https://assure-meaningful-purpose-green.trycloudflare.com/api',
+  // apiUrl: 'http://localhost:8080/api',
+
 };
