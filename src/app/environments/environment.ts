91b4733d782226ca0231592d27a2f376ef9bafec
JuAnHePo <juanantonioherreraponferrada@gmail.com>
2026-05-21 20:08:36 +0200

    cambio de url

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-   apiUrl: 'https://baltimore-milk-quantitative-dry.trycloudflare.com/api',
+   apiUrl: 'https://pharmaceutical-bathrooms-track-halifax.trycloudflare.com',
   // apiUrl: 'http://localhost:8080/api',
 
 };
